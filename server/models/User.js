f5a3b4ae5e7a7f3a94111435d9f0b68ae795b847
jlfuertes14 <johnlester.fuertes@gmail.com>
2025-11-24 16:23:41 +0800

    Fix Mongoose async pre-save hook

--- server/models/User.js
+++ server/models/User.js
@@ -33,10 +33,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+// Hash password before saving
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 });
 
 // Method to compare passwords
